1ba8da6b4fea09f70260922ad050e93b82eee53d
Kunal Gautam <141932830+Kunalchandra966@users.noreply.github.com>
2026-05-26 00:19:09 +0530

    Refactor service worker to use cache versioning

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,37 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('honeycalc').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js'
-      ]);
-    })
+const CACHE_NAME = "honeycalc-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+
   );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+
+  );
+
 });
